4ab0d5835f1a3afa238d7f547f9c16e5f130eef1
Mike Rizzo <rizblie@gmail.com>
2020-09-16 10:41:46 +0100

    My tap with Hugo 0.74.3

---
+++ Formula/hugo@0.74.3.rb
@@ -0,0 +1,36 @@
+class HugoAT0743 < Formula
+  desc "Configurable static site generator"
+  homepage "https://gohugo.io/"
+  url "https://github.com/gohugoio/hugo/archive/v0.74.3.tar.gz"
+  sha256 "9b296fa0396c20956fa6a1f7afadaa78739af62c277b6c0cfae79a91b0fe823f"
+  license "Apache-2.0"
+  head "https://github.com/gohugoio/hugo.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+    (buildpath/"src/github.com/gohugoio/hugo").install buildpath.children
+
+    cd "src/github.com/gohugoio/hugo" do
+      system "go", "build", "-o", bin/"hugo", "-tags", "extended", "main.go"
+
+      # Build bash completion
+      system bin/"hugo", "gen", "autocomplete", "--completionfile=hugo.sh"
+      bash_completion.install "hugo.sh"
+
+      # Build man pages; target dir man/ is hardcoded :(
+      (Pathname.pwd/"man").mkpath
+      system bin/"hugo", "gen", "man"
+      man1.install Dir["man/*.1"]
+
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    site = testpath/"hops-yeast-malt-water"
+    system "#{bin}/hugo", "new", "site", site
+    assert_predicate testpath/"#{site}/config.toml", :exist?
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Rizblie Mytap
+
+## How do I install these formulae?
+`brew install rizblie/mytap/<formula>`
+
+Or `brew tap rizblie/mytap` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
